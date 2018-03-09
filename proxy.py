5bf1043631750a50670924dfa665db040ba12f25
Irvan Fathul Huda <irvanfathul@gmail.com>
2018-03-09 10:54:22 +0700

    update notification on proxy server

--- proxy.py
+++ proxy.py
@@ -10,17 +10,21 @@ s2.connect("tcp://127.0.0.1:4445")
 # server2
 
 class Hitung(object):
-    def calc(self, x, y, op):
+    def calc(self, x, y, op): 
         if (op == 1): #tambah
+            print "Memproses Penjumlahan -> Di Server 1"
             return s1.tambah(x,y)
         elif (op == 2): #kurang
+            print "Memproses Pengurangan -> Di Server 1"
             return s1.kurang(x,y)
         elif (op == 3): #bagi
+            print "Memproses Pembagian -> Di Server 2"
             return s2.pembagian(x,y)
         elif (op == 4): #kali
+            print "Memproses Perkalian -> Di Server 2"
             return s2.perkalian(x,y)
         else:
-            return 0
+            return 0     
 
 s = zerorpc.Server(Hitung())
 s.bind("tcp://0.0.0.0:4242")
